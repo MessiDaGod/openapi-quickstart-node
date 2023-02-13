1ec94c53773de2a97d6c21f53c96fd302ac5559a
joeshakely <joeshakely@gmail.com>
2023-02-13 13:00:24 -0800

    top bar

--- src/pages/TopBar.jsx
+++ src/pages/TopBar.jsx
@@ -1,12 +1,22 @@
-import Link from "next/link";
-import styles from "./TopBar.module.css";
-import Dropdown from "./dropdown";
+import React from 'react';
+import styles from './TopBar.module.css';
+import Dropdown from './Dropdown';
 
-const TopBar = () => {
+const TopBar = ({ jsonFileName }) => {
   return (
-    <nav className={styles.TopBar}>
-      <Dropdown jsonFileName="data" />
-    </nav>
+    <div className={styles.topbar}>
+      <div className={styles.logo}>
+        <a href="/">
+          <img src="dog.png" alt="Dog Logo" />
+        </a>
+      </div>
+      <div className={styles.links}>
+        <a href="/">Home</a>
+        <a href="/">Features</a>
+        <a href="/">Pricing</a>
+      </div>
+      <Dropdown jsonFileName={jsonFileName} />
+    </div>
   );
 };
 
